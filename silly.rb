8acdc05d8556248794e719d1cddda32404e3974d
madeline57 <maddiejobeth@aol.com>
2018-07-24 14:09:11 +0000

    automatically backed up by learn

---
+++ silly.rb
@@ -0,0 +1,14 @@
+puts "daisy"
+favrorite = "daisy"
+puts favrorite.upcase
+
+puts "IM NOT SHOUTING"
+a = "IM NOT SHOUTING"
+puts a.downcase
+
+puts "supercalifragilisticexpialidocious"
+b = "supercalifragilisticexpialidocious"
+
+
+puts "wow" +"mom"
+c = "wow" ="mom"
